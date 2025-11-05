c38dc38315376102eac06368abdd651e87f3eb91
uriel-salazar <urielsalazar.dev@gmail.com>
2025-11-05 14:58:25 -0600

    Refactor main menu and playlist functionality; remove unused files and improve user interaction

--- search_playlist/playlist.py
+++ search_playlist/playlist.py
@@ -1,13 +1,16 @@
-from spotify_api.list_songs import get_scopes
+from spotify_api.list_songs import get_scopes,display_playlist
 
 def menu_playlist():
+    """ Menu specialized in playlists (delete/search/add)
+    """
     while True:
         print("1. Search your playlist")
         print("2. Add songs to your playlist ")
         print("3. Delete a playlist ")
-        option=input("Select an option :")
+        option=int(input("Select an option :"))
         if option==1:
             get_scopes()
+            playlist=display_playlist()
         elif option==2:
             pass
         elif option==3:
