535bee36c33939a605a791c47df1409086ccefb4
uriel-salazar <urielsalazar.dev@gmail.com>
2025-11-11 14:48:19 -0600

    Clean tracking and reapply .gitignore

--- basic menu/menu_main.py
+++
@@ -1,7 +0,0 @@
-
-def main_menu():
-    while True:
-        print("1. Search your playlist")
-        print("2. Eliminate playlist ")
-        print("3. Playlists saved")
-        option=input("Select an")
