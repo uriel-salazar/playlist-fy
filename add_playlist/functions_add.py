4fff8c6f06cf159b3069c7d208ae567550c0de54
uriel-salazar <urielix987@gmail.com>
2025-11-13 19:20:57 -0600

    add function for searching songs, extracting raw dict from song chosen,

--- add_playlist/functions_add.py
+++ add_playlist/functions_add.py
@@ -6,6 +6,20 @@ def get_uri():
 
 
 
-def search_songs():
-    pass
+def ask_songs(sp):
+    """ Ask to user for songs to add 
+
+    Args:
+        sp (spotipy): SpotifyOAuth 
+
+    Returns:
+        results(spotipy): Raw dictionary of available tracks 
+    """
+    search=input("What song you want to search?:")
+    results = sp.search(q=search, type="track", limit=7)
+    print(results)
+    return results 
+
+
+    
     
