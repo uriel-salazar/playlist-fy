fad6644616fafb8b8348c831d6524951540e0bd0
uriel-salazar <urielsalazar.dev@gmail.com>
2025-11-16 19:02:14 -0600

    trying to get the variable of uri's playlist to another file..

--- add_playlist/add_functions.py
+++ add_playlist/add_functions.py
@@ -1,5 +1,6 @@
 from get_data.call import current_playlist
-from add_playlist.functions_add import available,chosen
+from add_playlist.functions_add import available,chosen,add
+from get_data.interact_user import extract_dict
 
 def wrap_songs():
     sp,playlists=current_playlist()
@@ -7,7 +8,8 @@ def wrap_songs():
         index,track,track_list,song_chosen=available(sp)
         change_song,uri_song=chosen(song_chosen)
         if change_song=="yes":
-            pass
+            _,_,_,_,_,uri_playlist=extract_dict()
+            add(uri_playlist,uri_song)
             break
         else:
             continue
