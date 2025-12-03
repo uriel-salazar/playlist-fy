55a8db90e5659093cb3cf7c46fed4c3994d5da28
uriel-salazar <urielsalazar.dev@gmail.com>
2025-12-03 17:15:06 -0600

    For loop for top songs completed

--- user_top/song.py
+++ user_top/song.py
@@ -1,7 +1,15 @@
 from api_data.call import current_playlist
 
 def top_songs():
+    """
+    Displays top tracks (10 songs) to the user.
+    """
     sp,_,_=current_playlist()
-    tier_song=sp.current_user_top_tracks(limit=10,offset=0,time_range="long_term")
-    print(tier_song)
+    tier_song=sp.current_user_top_tracks(limit=10,offset=0,time_range="medium_term")
+    song_10=tier_song
+    for num,song in enumerate(tier_song["items"],start=1):
+        song_10=song["name"]
+        print(f"-{num} {song_10}")
+        
+        
                     
