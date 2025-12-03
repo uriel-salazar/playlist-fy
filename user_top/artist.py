c2d3365bb30dc0dbe5f2962c3a70939159ad4fd2
uriel-salazar <urielsalazar.dev@gmail.com>
2025-12-02 18:58:35 -0600

    Docstring created  for top_artist function

--- user_top/artist.py
+++ user_top/artist.py
@@ -1,6 +1,12 @@
 from api_data.call import current_playlist
 def top_artist():
+    """ Prints User's top artist in a list (10)
+    (medium term top )
+    """               
     sp,_,_=current_playlist()
-    tier_artist=sp.current_user_top_artists(limit=30,time_range="long_term")
-    for singer in tier_artist["items"]:
-        print(singer["name"])
+    tier_artist=sp.current_user_top_artists(limit=10,time_range="medium_term")
+    print("Your Top artists :")
+    for num,singer in enumerate(tier_artist["items"],start=1):
+        artist_10=singer["name"]
+        print(f'- {num} {artist_10}')
+        
