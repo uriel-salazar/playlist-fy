e081cd3e4982cb71767aa7773f2186e1b5e8649e
uriel-salazar <urielsalazar.dev@gmail.com>
2025-12-09 17:39:18 -0600

    completing dosctrings part 2

--- user_top/song.py
+++ user_top/song.py
@@ -8,12 +8,15 @@ def top_songs():
      Otherwise, gets the current user tracks 
     """
     sp,_,_=current_playlist()
+    
     if sp!=None:   
         tier_song=sp.current_user_top_tracks(limit=10,offset=0,time_range="long_term")
+        
         song_10=tier_song
         print("* Top songs *")
         for num,song in enumerate(tier_song["items"],start=1):
             song_10=song["name"]
+            
             print(f"-{num} {song_10}")
     else:
         return
